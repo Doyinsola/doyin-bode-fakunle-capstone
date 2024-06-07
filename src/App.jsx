00efb71aa087ae6f6e0b82d938815382d955d66e
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2024-06-07 10:38:22 -0400

    added pages and routing to those pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import ContentDetails from './pages/ContentDetails/ContentDetails';
+import CategoryContent from './pages/CategoryContent/CategoryContent';
+import NotFound from './pages/NotFound/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}
+          />
+          <Route path='categories/:id' element={<CategoryContent />}
+          />
+          <Route path='/content/:id' element={<ContentDetails />}
+          />
+          <Route path='*' element={<NotFound />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
