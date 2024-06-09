7f1c782acf7a85041573864f734e66f1a3a9109a
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2024-06-09 17:31:06 -0400

    added API calls to like content, retrieve, like and delete comments, and for login/sign up

--- src/classes/apo-imo-api.js
+++ src/classes/apo-imo-api.js
@@ -19,4 +19,28 @@ export default class ApoImoApi {
     async getContentDetails(contentID) {
         return axios.get(this.serverURL + "/api/content/" + contentID);
     }
+    async likeContent(contentID) {
+        return axios.patch(this.serverURL + "/api/content/" + contentID + "/like")
+    }
+    async getComments(contentID) {
+        return axios.get(this.serverURL + "/api/content/" + contentID + "/comments")
+    }
+    async postComments(contentID) {
+        return axios.post(this.serverURL + "/api/content/" + contentID + "/comments")
+    }
+    async likeComment(contentID, commentID) {
+        return axios.patch(this.serverURL + "/api/content/" + contentID + "/comments/" + commentID + "/like")
+    }
+    async deleteComment(contentID, commentID) {
+        return axios.delete(this.serverURL + "/api/content/" + contentID + "/comments/" + commentID)
+    }
+    async signUp() {
+        return axios.post(this.serverURL + "/api/user/register")
+    }
+    async login() {
+        return axios.post(this.serverURL + "/api/user/login")
+    }
+    async getUserProfile() {
+        return axios.gett(this.serverURL + "/api/user/profile")
+    }
 }
