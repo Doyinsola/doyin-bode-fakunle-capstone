3cae9b947899dec494457ad28a9d12dfba5d9f61
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2024-06-09 09:34:31 -0400

    styled login and sign up pages accross breakpoints

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,7 +1,38 @@
 import './LogIn.scss';
+import FormField from '../../components/FormField/FormField';
+import CTA from '../../components/CTA/CTA';
+import { Link } from "react-router-dom";
 
 function LogIn() {
 
+    return (
+        <main className='login'>
+            <section className='login__container'>
+                <h1 className='login__heading'>Welcome Back!</h1>
+                <form className='login__form'>
+                    <div className='login__email-container'>
+                        <h2 className='login__email labels'>Email</h2>
+                        <FormField
+                            type="email"
+                            name="email"
+                            placeholder="email"
+                            componentClass="login__email--input" />
+                    </div>
+                    <div className='login__password-container'>
+                        <h2 className='login__password labels'>Password</h2>
+                        <FormField
+                            type="password"
+                            name="password"
+                            placeholder="password"
+                            componentClass="login__password--input" />
+                    </div>
+                    <CTA componentClass="login__button"
+                        componentText="Log In" />
+                    <p className='login__signup-text'>Don't have an account? <Link to='/signup' className='login__signup-link'>Sign Up</Link></p>
+                </form>
+            </section>
+        </main>
+    )
 }
 
 export default LogIn;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -6,46 +6,49 @@ import { Link } from "react-router-dom";
 function SignUp() {
 
     return (
-        <main>
-            <h1>Welcome!</h1>
-            <form>
-                <div>
-                    <div>
-                        <h2>First Name</h2>
-                        <FormField
-                            type="text"
-                            name="firstName"
-                            placeholder="first name"
-                            componentClass="" />
+        <main className='sign-up'>
+            <section className='sign-up__container'>
+                <h1 className='sign-up__heading'>Sign up to Apó Imọ</h1>
+                <form className='sign-up__form'>
+                    <div className='sign-up__name-container'>
+                        <div className='sign-up__firstname-container'>
+                            <h2 className='sign-up__firstname labels'>First Name</h2>
+                            <FormField
+                                type="text"
+                                name="firstName"
+                                placeholder="first name"
+                                componentClass="sign-up__firstname--input" />
+                        </div>
+                        <div className='sign-up__lastname-container'>
+                            <h2 className='sign-up__lastname labels'>Last Name</h2>
+                            <FormField
+                                type="text"
+                                name="LastName"
+                                placeholder="last name"
+                                componentClass="sign-up__lastname--input" />
+                        </div>
                     </div>
-                    <div>
-                        <h2>Last Name</h2>
+                    <div className='sign-up__email-container'>
+                        <h2 className='sign-up__email labels'>Email</h2>
                         <FormField
-                            type="text"
-                            name="LastName"
-                            placeholder="last name"
-                            componentClass="" />
-                    </div></div>
-                <div>
-                    <h2>Email</h2>
-                    <FormField
-                        type="email"
-                        name="email"
-                        placeholder="email"
-                        componentClass="" />
-                </div>
-                <div>
-                    <h2>Password</h2>
-                    <FormField
-                        type="password"
-                        name="password"
-                        placeholder="password"
-                        componentClass="" />
-                </div>
-                <CTA componentClass=""
-                    componentText="Create Account" />
-                <p>Already have an account? <Link to='/login'>Log In</Link></p>
-            </form>
+                            type="email"
+                            name="email"
+                            placeholder="email"
+                            componentClass="sign-up__email--input" />
+                    </div>
+                    <div className='sign-up__password-container'>
+                        <h2 className='sign-up__password  labels'>Password</h2>
+                        <FormField
+                            type="password"
+                            name="password"
+                            placeholder="password"
+                            componentClass="sign-up__password--input" />
+                    </div>
+                    <CTA componentClass="sign-up__button"
+                        componentText="Create Account" />
+                    <p className='sign-up__login-text'>Already have an account? <Link to='/login' className='sign-up__login-link'>Log In</Link></p>
+                </form>
+            </section>
         </main>
     )
 }
