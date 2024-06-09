6b1f1979c645642773492d611a097e386abed1be
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2024-06-09 00:51:22 -0400

    added components to the sign uppage

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,53 @@
 import './SignUp.scss';
+import FormField from '../../components/FormField/FormField';
+import CTA from '../../components/CTA/CTA';
+import { Link } from "react-router-dom";
 
 function SignUp() {
 
+    return (
+        <main>
+            <h1>Welcome!</h1>
+            <form>
+                <div>
+                    <div>
+                        <h2>First Name</h2>
+                        <FormField
+                            type="text"
+                            name="firstName"
+                            placeholder="first name"
+                            componentClass="" />
+                    </div>
+                    <div>
+                        <h2>Last Name</h2>
+                        <FormField
+                            type="text"
+                            name="LastName"
+                            placeholder="last name"
+                            componentClass="" />
+                    </div></div>
+                <div>
+                    <h2>Email</h2>
+                    <FormField
+                        type="email"
+                        name="email"
+                        placeholder="email"
+                        componentClass="" />
+                </div>
+                <div>
+                    <h2>Password</h2>
+                    <FormField
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        componentClass="" />
+                </div>
+                <CTA componentClass=""
+                    componentText="Create Account" />
+                <p>Already have an account? <Link to='/login'>Log In</Link></p>
+            </form>
+        </main>
+    )
 }
 
 export default SignUp;
