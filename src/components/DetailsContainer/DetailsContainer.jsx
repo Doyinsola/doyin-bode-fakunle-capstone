4d806461e41520b9ca061723907d48c670b2b2c7
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2024-06-09 22:09:12 -0400

    added style to active category button

--- src/components/DetailsContainer/DetailsContainer.jsx
+++ src/components/DetailsContainer/DetailsContainer.jsx
@@ -1,14 +1,11 @@
 import './DetailsContainer.scss';
 import likesIcon from '../../assets/icons/likes.svg';
 import CTA from '../CTA/CTA';
-import { Link } from 'react-router-dom';
-import ApoImoApi from '../../classes/apo-imo-api';
 
 function DetailsContainer({
     contentDetails,
     clickHandler
 }) {
-
     return (
         <section className="details__container">
             <h2 className='details__name'>Name: {contentDetails.name}</h2>
